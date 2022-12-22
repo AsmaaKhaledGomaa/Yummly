bd8066aa3b5e5c7ea3276ce81cf332e6bb01c97c
asmaakhaledgomaa <asmaa.kh.gomaa@gmail.com>
2022-12-22 18:03:00 +0200

    shimmer

--- app/src/main/java/com/asmaa/yummly/RecipeFragment.kt
+++ app/src/main/java/com/asmaa/yummly/RecipeFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 
 
 class RecipeFragment : Fragment() {
@@ -16,7 +15,7 @@ class RecipeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_recipe, container, false)
-        view.recyclerview.showShimmer()
+       // view.recyclerview.showShimmer()
         return view
     }
 }
